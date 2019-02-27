4171d8691bd7f2d856f88bab8eda4e6783c3103b
Scott Varcoe <scottv@interknowlogy.com>
2019-02-27 15:45:46 -0800

    initial project with null reference checking

---
+++ src/Csharp8Demo.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Csharp8Demo.ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!!");
+            Console.Beep()  ;
+
+            string s = null;
+
+            Console.WriteLine($"Hi {s}");
+        }
+    }
+}
